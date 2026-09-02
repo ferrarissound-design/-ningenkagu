dc14724c5f7531e7410eecace1430326857e0609
ferrarissound-design <ferrarissound@gmail.com>
2026-09-02 11:19:03 +0900

    refactor: load stage event extensions centrally

--- js/stage.js
+++ js/stage.js
@@ -3,6 +3,7 @@ import { rectDistance, disposeObject3D } from './utils.js';
 import { ROOM } from './stageBuilder.js';
 import { getStageDefinition } from './stageRegistry.js';
 import { POSE_FOR_KIND } from './furnitureKinds.js';
+import './stageEventExtensions.js';
 
 export { ROOM, POSE_FOR_KIND };
 
